826507e2aecfc8f6f123371c5a9484b8a655e579
kristjan0 <1533424+kristjan0@users.noreply.github.com>
2022-11-29 15:43:35 +0200

    Ideed.txt ja paar rida koodi

--- uusvers.py
+++ uusvers.py
@@ -1,6 +1,16 @@
 import os
 from datetime import datetime
 
+# class ylesanne:
+# 	def __init__(self, nimi, kuu, paev, aasta, tund, minut):
+# 		self.nimi = nimi
+# 		self.kuu = kuu
+# 		self.paev = paev
+# 		self.aasta = aasta
+# 		self.tund = tund
+# 		self. minut = minut
+
+
 failinimi = "andmed.txt"
 #true või false
 eksisteerib = os.path.exists(failinimi)
@@ -12,46 +22,47 @@ with open(failinimi, "a" if eksisteerib else "w", encoding="UTF-8") as f:
 			# Mõtlesin äkki võtta see kuupäev tükkideks lahti, siis äkki kergem sorteerida neid 
 			# Põhimõtteliselt saab selle 'kuu,paev,aasta' osa muuta tagasi MM-DD-YY formaati faili kirjutamisel.
 			# Selle asjaga saab lihtsalt checkida kas kuupäev on normaalne.
-			# 
-			# kuu,paev,aasta = input("Sisesta kuupäev(MM-PP-YY): ").split("-")
+			#
+			# kuupaev = input("Sisesta kuupäev(MM-PP-YY): ") #Algne
+			# kuu,paev,aasta = kuupaev.split("-")
 			# kuu,paev,aasta = int(kuu),int(paev),int(aasta)
-			# aasta = int("20"+str(aasta))
+			# aasta = int("20"+str(aasta).zfill(2))
 			# if int(kuu) in [1,3,5,7,8,10,12]:
 			# 	if int(paev) > 0 and int(paev) <=31:
 			# 		continue
 			# 	else:
-			# 		raise ValueError(f"Teie sisestatud kuupäev {(str(kuu).zfill(2))}-{paev}-{(str(aasta)[-2:])} ei ole korrektne")
+			# 		raise ValueError(f"Teie sisestatud kuupäev {kuupaev} ei ole korrektne")
 			# elif int(kuu) in [4,6,9,11]:
 			# 	if paev > 0 and paev <=30:
 			# 		continue
 			# 	else:
-			# 		raise ValueError(f"Teie sisestatud kuupäev {str(kuu).zfill(2)}-{paev}-{(str(aasta)[-2:])} ei ole korrektne")
+			# 		raise ValueError(f"Teie sisestatud kuupäev {kuupaev} ei ole korrektne")
 			# elif int(kuu) == 2:
 			# 	if (aasta % 400 == 0):
 			# 		if paev > 0 and paev <= 29:
 			# 			continue
 			# 		else:
-			# 			raise ValueError(f"Teie sisestatud kuupäev {str(kuu).zfill(2)}-{paev}-{(str(aasta)[-2:])} ei ole korrektne")
+			# 			raise ValueError(f"Teie sisestatud kuupäev {kuupaev} ei ole korrektne")
 			# 	elif (aasta % 100 != 0) and (aasta % 4 == 0):
 			# 		if paev > 0 and paev <= 29:
 			# 			continue
 			# 		else:
-			# 			raise ValueError(f"Teie sisestatud kuupäev {(str(kuu).zfill(2))}-{paev}-{(str(aasta)[-2:])} ei ole korrektne")
+			# 			raise ValueError(f"Teie sisestatud kuupäev {kuupaev} ei ole korrektne")
 			# 	else:
 			# 		if paev > 0 and paev <= 28:
 			# 			continue
 			# 		else:
-			# 			raise ValueError(f"Teie sisestatud kuupäev {(str(kuu).zfill(2))}-{paev}-{(str(aasta)[-2:])} ei ole korrektne")
+			# 			raise ValueError(f"Teie sisestatud kuupäev {kuupaev} ei ole korrektne")
 			# else:
-			# 	raise ValueError(f"Teie sisestatud kuupäev {(str(kuu).zfill(2))}-{paev}-{(str(aasta)[-2:])} ei ole korrektne")
+			# 	raise ValueError(f"Teie sisestatud kuupäev {kuupaev} ei ole korrektne")
 			# aasta = int(str(aasta)[-2:])
 			#
 			kuupaev = input("Sisesta kuupäev(MM-PP-YY): ") #Algne
 			kellaaeg = input("Sisesta kellaaeg(00:00): ")
 			f.write(f"Ülesanne: {sisestus}, Kuupäev: {kuupaev}, Kellaaeg: {kellaaeg}\n")  #Algne
-			# f.write(f"Ülesanne: {sisestus}, Kuupäev: {(str(kuu).zfill(2))}-{paev}-{aasta}, Kellaaeg: {kellaaeg}\n") #Tagasi MM-DD-YY formaati
+			# f.write(f"Ülesanne: {sisestus}, Kuupäev: {(str(kuu).zfill(2))}-{paev}-{aasta}, Kellaaeg: {kellaaeg}\n") #Tagasi MM-DD-YY formaati. Pole vaja, tuli parem idee
 			jatkuvus = input("Tahad veel ülesandeid lisada(y - jah, n - ei)? ")
-			if jatkuvus == "n":
+			if jatkuvus.lower() == "n":
 				break
 			else:
 				continue
