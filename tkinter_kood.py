9027502c924787de12034e5818a6beecaf4a569a
RobinT6n <robint6niste@gmail.com>
2022-12-14 15:27:27 +0200

    uuendus

--- tkinter_kood.py
+++ tkinter_kood.py
@@ -33,7 +33,7 @@ while True:
 	root.title("Ülesande sisestus")
 	sisestus = tkinter.Entry(root, width=50)
 	sisestus.pack()
-	nupp = tkinter.Button(root, text="Lisa ulesanne", width=48, command=nupu_vajutus_ulesanne)
+	nupp = tkinter.Button(root, text="Lisa ülesande nimi", width=48, command=nupu_vajutus_ulesanne)
 	nupp.pack()
 	root.mainloop()
 
@@ -41,7 +41,7 @@ while True:
 	root2.title("kuupaeva sisestus") 
 	sisestus2 = tkinter.Entry(root2, width=50) #See võiks olla kohe eraldi lahtritena - Päev, kuu, aasta
 	sisestus2.pack()
-	nupp2 = tkinter.Button(root2, text="Lisa kuupäev", width=48, command=nupu_vajutus_kuupaev)
+	nupp2 = tkinter.Button(root2, text="Lisa kuupäev (MM-PP-YY)", width=48, command=nupu_vajutus_kuupaev)
 	nupp2.pack()
 	root2.mainloop()
 
@@ -49,7 +49,7 @@ while True:
 	root3.title("kellaaja sisestus")
 	sisestus3 = tkinter.Entry(root3, width=50) #See võiks ka olla kaks lahtrit - Tund, minut
 	sisestus3.pack()
-	nupp3 = tkinter.Button(root3, text="Lisa kellaaeg", width=48, command=nupu_vajutus_kellaaeg)
+	nupp3 = tkinter.Button(root3, text="Lisa kellaaeg (00:00)", width=48, command=nupu_vajutus_kellaaeg)
 	nupp3.pack()
 	root3.mainloop()
 
@@ -57,7 +57,6 @@ while True:
 
 	root4 = Tk()
 	root4.title("Jätkuvus")
-	user_password = Label(root4, text = "Password")
 	nuppjah = tkinter.Button(root4, text="Jätka", width=48, command=nupu_vajutus_jah)
 	nuppjah.pack()
 	nuppei = tkinter.Button(root4, text="katkesta", width=48, command=nupu_vajutus_ei)
@@ -67,5 +66,5 @@ while True:
 		#siia peaks nt lisama selle, et kui fail olemas, ss lisab faili ("a"), muidu kui pole faili olemas, siis kirjutab uue faili("w")
 		with open("andmete_fail.txt", "w", encoding="utf8") as f:
 			for i in andmed:
-				f.write(i)
+				f.write(f"Ülesanne: {i[0]}, kuupäev: {i[1]}, kellaaeg: {i[2]}")
 		break
